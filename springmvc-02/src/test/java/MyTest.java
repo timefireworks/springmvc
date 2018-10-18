1941965bbc5beab1452539b3ec5daba94c09d2d7
timefireworks <1158388143@qq.com>
2018-10-18 09:31:37 +0800

    修改pom

---
+++ springmvc-02/src/test/java/MyTest.java
@@ -0,0 +1,20 @@
+package com.test;
+
+/**
+ * author: San Jinhong
+ * date: 2018/9/27 14:41
+ **/
+public class MyTest {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Object object = new Object();
+        System.out.println(object.getClass().getClassLoader());
+
+        Class clazz = Class.forName("com.test.Hello");
+        System.out.println(clazz.getClassLoader());
+
+        MyTest myTest  = new MyTest();
+        System.out.println(myTest.getClass().getClassLoader());
+
+
+    }
+}
